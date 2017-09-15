772d3771263f4305715bafd11e316c58e7e0157c
rgrannell1 <r.grannell2@gmail.com>
2017-09-15 21:28:30 +0100

    Added asset cache script, temporary favicon

---
+++ src/client/core/asset-cache.js
@@ -0,0 +1,98 @@
+
+'use strict'
+
+
+
+
+
+const constants = {
+	cacheUrls: [
+		'css/polonium.css'
+	],
+	contentTypes: {
+		html: 'text/html'
+	},
+	caches: {
+		offline: 'offline-v1'
+	}
+}
+
+
+
+
+const cacheBustedRequest = url => {
+	return new Request(url, {cache: 'reload'})
+}
+
+const isCacheable = request => {
+
+	return false
+
+}
+
+
+
+
+
+
+const listeners = { }
+
+listeners.populateCache = async event => {
+
+	const cache = await caches.open(constants.caches.offline)
+	await cache.addAll(constants.cacheUrls)
+
+	return event.waitUntil(self.skipWaiting( ))
+
+}
+
+listeners.tidyCache = async event => {
+
+	const expectedCaches = Object.keys(constants.caches).map(cacheName => {
+		return constants.caches[cacheName]
+	})
+
+	const cacheNames     = await caches.keys( )
+	const deletePromises = cacheNames.map(cacheName => {
+
+		if (expectedCaches.indexOf(cacheName) === -1) {
+
+			console.log('deleting out of date cache.')
+			return caches.delete(cacheName)
+
+		}
+
+	})
+
+	return Promise.all(deletePromises)
+
+}
+
+listeners.serveFromCache = async event => {
+
+	if (isCacheable(event.request)) {
+
+		const cachedResponse = await caches.match(event.request)
+
+		if (cachedResponse) {
+			return cachedResponse
+		}
+
+		const cache = await caches.open(constants.caches.offline)
+
+		const uncachedResponse = await fetch(event.request)
+		await cache.put(event.request, uncachedResponse.clone( ))
+
+		return uncachedResponse
+
+	}
+
+}
+
+
+
+
+
+self.addEventListener('install',  listeners.populateCache)
+self.addEventListener('activate', listeners.tidyCache)
+self.addEventListener('fetch',    listeners.serveFromCache)
