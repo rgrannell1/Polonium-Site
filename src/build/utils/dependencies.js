d95d4f1d07fea6440c03eb615bd998db7244b280
rgrannell1 <r.grannell2@gmail.com>
2017-09-20 22:08:53 +0100

    added path dependency

--- src/build/utils/dependencies.js
+++ src/build/utils/dependencies.js
@@ -5,6 +5,25 @@
 
 
 const execa = require('execa')
+const chalk = require('chalk')
+const fs = require('fs')
+const {Enum} = require('enumify')
+
+
+
+class ReportState extends Enum { }
+
+ReportState.initEnum([
+	'FAILED',
+	'PASSED',
+	'SKIPPED',
+	'UNCERTAIN'
+])
+
+
+
+const ok = Promise.resolve
+const fail = Promise.reject
 
 
 
@@ -12,27 +31,44 @@ const execa = require('execa')
 
 const deps = { }
 
-
-
-
-
 deps.check = schemas => {
 
 	var hasError = false
 
-	return schemas.map(schema => {
+	const schemaResults = schemas.map(schema => {
 
-		schema
-			.then(( ) => {
-
+		schema.report( )
+			.then(message => {
+				console.log(message)
 			})
-			.catch(( ) => {
-
-			})
-
+			.catch(( ) => { })
 
 	})
+
+	return Promise.all(schemaResults)
+
 }
+
+
+
+
+const reportStatus = (ctx, message) => {
+
+	return ctx.status( ).then(report => {
+
+		switch (report.status) {
+			case ReportState.PASSED:
+				return `${ chalk.green('✓') } ${ message }`
+				break
+			case ReportState.FAILED:
+				return `${ chalk.red('❌') } ${ message }`
+				break
+		}
+
+	})
+
+}
+
 
 
 
@@ -41,25 +77,47 @@ class ExecutableDependency {
 	constructor (config) {
 		this.config = config
 	}
+	status ( ) {
+
+		return execa.shell(`which "${ this.config.name }"`)
+			.then( ( )  => ({status: ReportState.PASSED}) )
+			.catch( ( ) => ({status: ReportState.FAILED}) )
+
+	}
 	report ( ) {
+		return reportStatus(this, `executable: ${ this.config.name }`)
+	}
+}
 
-		this.config.names.map(name => {
-			return execa.shell(`which "${name}"`)
-				.catch(err => {
+class PathDependency {
+	constructor (config) {
+		this.config = config
+	}
+	status ( ) {
 
-					if (err.code === 1) {
-						throw new Error(`command "${name}" has no 'which' entry.`)
-					}
+		return new Promise((resolve, reject) => {
+			fs.stat(this.config.path, err => {
 
-				})
+				err
+					? resolve({status: ReportState.FAILED})
+					: resolve({status: ReportState.PASSED})
+
+			})
 		})
 
+	}
+	report ( ) {
+		return reportStatus(this, `path: ${ this.config.path }`)
 	}
 }
 
 
 
 
+deps.Executable = ExecutableDependency
+deps.Path = PathDependency
 
-```
+
+
+
 module.exports = deps
