04182784ef6a8bf7abd4639af7635375a02dc067
rgrannell1 <r.grannell2@gmail.com>
2017-11-27 00:33:35 +0000

    added screenshot generation

---
+++ tests/screenshot.js
@@ -0,0 +1,43 @@
+
+const path = require('path')
+
+const constants = {
+  devices: {
+    galaxyS5: {width: 360, height: 640},
+    nexus5x: {width: 412, height: 732},
+    xps: {width: 1880, height: 820}
+  }
+}
+
+const puppeteer = require('puppeteer')
+const config = require('config')
+
+const screenshot = async (prefix, device, setup) => {
+  const browser = await puppeteer.launch({
+    ignoreHTTPSErrors: true,
+    headless: false
+  })
+
+  const {width, height} = constants.devices[device]
+
+  const page = await browser.newPage()
+  await page.goto(`https://${config.get('vm.subDomain')}.${config.get('vm.domain')}`)
+  page.setViewport({width, height})
+
+  if (setup) {
+    await setup(browser, page)
+  }
+
+  await page.screenshot({
+    path: path.join(__dirname, '../screenshots', `${prefix}-${device}.png`)
+  })
+
+  await browser.close()
+}
+
+Object.keys(constants.devices).forEach(async device => {
+  await screenshot('screen-load', device)
+  await screenshot('click', device, async (browser, page) => {
+    await page.click('#links')
+  })
+})
