859361cbfd7ba315bf5b0b2964e1c7d417a40e6d
rgrannell1 <r.grannell2@gmail.com>
2017-12-21 18:43:28 +0000

    added 404 page

--- src/server/routes/get/not-found.js
+++ src/server/routes/get/not-found.js
@@ -4,28 +4,36 @@ require('mithril/test-utils/browserMock')(global)
 const m = require('mithril')
 const render = require('mithril-node-render')
 
-/**
- *
- * Response.
- *
- */
-module.exports = async (ctx, next) => {
-  const dom = m('html', {lang: 'en'},
-    m('head',
+const pages = {}
+pages.outline = () => {
+  const header = m('head',
       m('meta', {charset: 'utf-8'}),
       m('title', 'Polonium'),
       m('meta', {name: 'theme-color', content: '#5d51d6'}),
       m('viewport', {content: 'width=device-width, initial-scale=1'}),
       m('link', {rel: 'manifest', href: './manifest.json'}),
       m('link', {rel: 'stylesheet', href: 'css/polonium.min.css', media: 'all'})
-    ),
-    m('body#app',
-      m('.container')
     )
-  )
 
+  const body = m('body#app',
+      m('.container',
+        m('header#main-head',
+          m('label.burger-menu', {for: 'slide', title: 'Main menu'}, '☰'),
+          m('a', {href: '/#!/'},
+            m('h1#icon-brand', {class: 'brand'}, 'Polonium')),
+          m('label#links', {for: 'slide'}))))
+
+  return m('html', {lang: 'en'}, header, body)
+}
+
+/**
+ *
+ * Response.
+ *
+ */
+module.exports = async (ctx, next) => {
   if (httpResponses.is.NotFound(ctx)) {
-    Object.assign(ctx, httpResponses.NotFound({body: render(dom)}))
+    ctx.body = await render(pages.outline())
   }
 
   next()
